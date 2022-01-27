dc1b6357ce38eeeaa1bb8e5a06bc346796626f81
Lamprinos Chatziioannou <chatziiol@gmail.com>
2022-01-27 19:21:48 +0200

    CSS was not loading

--- build-site.el
+++ build-site.el
@@ -23,7 +23,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"/rougier.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"rougier.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
