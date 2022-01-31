81359e8329b417904b7a5df391f8f9f7693ba056
Lamprinos Chatziioannou <chatziiol@gmail.com>
2022-01-31 12:58:59 +0200

    Restructured the site a little bit, added sitelogo, navigation, and sitemap

--- build-site.el
+++ build-site.el
@@ -23,6 +23,11 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
+      org-html-link-home "index.html"
+      org-html-link-up "index.html"
+      org-html-indent nil
+      org-html-self-link-headlines t
+      ;; org-export-filter-body-functions (I just want to read a file)
       org-html-head "<link rel=\"stylesheet\" href=\"rougier.css\" />")
 
 ;; Define the publishing project
@@ -31,9 +36,13 @@
        (list "org-site:main"
              :recursive t
              :base-directory "./content"
+             :auto-sitemap t
+             :sitemap-filename "sitemap.org"
+             :sitemap-title ""
+             :sitemap-sort-files: 'anti-chronologically
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-             :with-author nil           ;; Don't include author name
+             :with-author t           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
