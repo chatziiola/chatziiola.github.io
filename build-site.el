92d5d04ba6f0919038c38d401745f497b80d766b
Lamprinos Chatziioannou <chatziiol@gmail.com>
2022-02-03 18:28:03 +0200

    Mitnick article written, added small indexes on the subfolders. Cleaner look

--- build-site.el
+++ build-site.el
@@ -24,29 +24,38 @@
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-link-home "/index.html"
-      org-html-link-up "/index.html"
+      org-html-link-up "index.html"
       org-html-indent nil
       org-html-self-link-headlines t
       ;; org-export-filter-body-functions (I just want to read a file)
       org-html-head "<link rel=\"stylesheet\" href=\"/rougier.css\" />")
 
 ;; Define the publishing project
+;; TODO, when you try more customizations here: check this https://ogbe.net/blog/blogging_with_org.html
 (setq org-publish-project-alist
       (list
        (list "org-site:main"
              :recursive t
              :base-directory "./content"
              :auto-sitemap t
-             :sitemap-filename "sitemap.org"
-             :sitemap-title ""
-             :sitemap-sort-files: 'anti-chronologically
+;             :sitemap-filename "sitemap.org"
+;             :sitemap-title ""
+;             :sitemap-sort-files: 'anti-chronologically
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
              :with-author t           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-drawers t
+             :headline-level 3
              :with-toc t                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             :time-stamp-file t)
+       (list "images"
+        :base-directory "./content/images"
+         :base-extension ".*"
+         :publishing-directory "./public/images"
+         :publishing-function 'org-publish-attachment)))
+
 
 ;; Generate the site output
 (org-publish-all t)
