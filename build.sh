293a2f3dd1eff4bbcb8b773021d237e5af94e414
Lamprinos Chatziioannou <chatziiol@gmail.com>
2023-01-11 09:42:39 +0200

    Comment on build.sh and fixed links on header.html

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ fi
 
 emacs -Q --script build-site.el
 
-# Add latest posts at the end of
+# Add latest posts at /index The reason why this was added here and not inside
+# of build-site.el is the fact that index.org is published before recents is
+# created and before enough html files exist
 echo "Adding mini archive on /index.html"
 echo '<ul class="org-ul indexul">' >/tmp/index
 grep "^<li>" public/posts/recents.html | head -5  >> /tmp/index
 echo '</ul>' >> /tmp/index
-# TODO make it add a ul environment as wel
 sed -i -e '/Latest Articles<\/a><\/h2>/r /tmp/index'  ./public/index.html
 
 # Copy /index.html to /posts/index.html
