017cdff9cc4908e02614dd1147037ba1d8d31b9a
Chatziioannou Lamprinos <chatziiol@gmail.com>
2024-12-22 00:28:01 +0200

    Added theme selection, revamped css

--- docs/src/copy-pre.js
+++ docs/src/copy-pre.js
@@ -49,25 +49,31 @@ document.addEventListener("DOMContentLoaded", function() {
     function addCopyButtonToPreElements() {
         // Get all pre elements
         var preElements = document.querySelectorAll('pre');
-        
+
         // Iterate over each pre element
         preElements.forEach(function(preElement) {
+
             // Create a button
             var copyButton = document.createElement('button');
-            copyButton.textContent = 'Copy';
             copyButton.classList.add('copy-button');
-            
+
+            // Create the Font Awesome icon element
+            var icon = document.createElement('i');
+            icon.classList.add('fa-regular', 'fa-clipboard'); // Add Font Awesome classes for the clipboard icon
+
+            // Append the icon to the button
+            copyButton.appendChild(icon);
+
             // Append the button to the pre element
             preElement.appendChild(copyButton);
-            
             // Add click event listener to the button
             copyButton.addEventListener('click', function() {
-		// Get the text content of the pre element
+                // Get the text content of the pre element
                 var contentToCopy = preElement.textContent.trim();
-                
+
                 // Remove the button's text content from the end
                 contentToCopy = contentToCopy.slice(0, -copyButton.textContent.length);
-                
+
                 navigator.clipboard.writeText(contentToCopy).then(function() {
                     console.log('Content copied to clipboard:', contentToCopy);
                 }, function(err) {
@@ -76,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     }
-    
+
     // Call the function to add copy buttons to pre elements
     addCopyButtonToPreElements();
 });
